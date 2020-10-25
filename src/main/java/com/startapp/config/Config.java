22f802aedaa06c357cadaf6620a0ac4515793c25
favorskij <favorskij@mail.ru>
2020-10-25 13:23:07 +1100

    Initial commit

---
+++ src/main/java/com/startapp/config/Config.java
@@ -0,0 +1,10 @@
+package com.startapp.config;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@ComponentScan("com.startapp.controller")
+public class Config {
+
+}
---
+++ src/main/java/com/startapp/controller/HomeController.java
@@ -0,0 +1,19 @@
+package com.startapp.controller;
+
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+public class HomeController {
+
+
+    @ResponseBody
+    @GetMapping(value = "/")
+    public String homePage () {
+            return "Hello World!!!";
+    }
+
+
+}
